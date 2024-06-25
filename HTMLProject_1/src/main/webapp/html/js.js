3a2eb3a1195dd5a276161235ca33246ea023f272
hhj71 <chhj017@naver.com>
2024-06-25 17:52:53 +0900

    0625 HTML 기본 태그

---
+++ HTMLProject_1/src/main/webapp/html/js.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+function start1(){
+		alert("자바스크립트 시작1");
+	}
+	let start2 = function() {
+		alert("자바스크립트 시작2")
+	}
+	// 자바스크립트 6버전에서 주로 사용 => Arrow 함수 => function, return 을 제거해서 사용
+	// 자바 => 람다식
+	let start3 = ()=> {
+		alert("자바스크립트 시작3")
+	}
