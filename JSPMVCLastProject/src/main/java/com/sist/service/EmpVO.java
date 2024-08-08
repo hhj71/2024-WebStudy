15e72e932a995a44b28c38934629e351cdd117f3
hhj71 <chhj017@naver.com>
2024-08-08 17:55:48 +0900

    0808

---
+++ JSPMVCLastProject/src/main/java/com/sist/service/EmpVO.java
@@ -0,0 +1,11 @@
+package com.sist.service;
+import java.util.*;
+
+import lombok.Data;
+
+@Data
+public class EmpVO {
+		private int empvo, sal;
+		private String ename, job;
+		private Date hiredate;
+}
